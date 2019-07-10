af8f7ad443a2ada3934fe322f498b91e796faa89
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-10 23:45:45 +0545

    Show number of movies message at the top of the table

--- src/components/movies.js
+++ src/components/movies.js
@@ -18,36 +18,42 @@ class Movies extends Component {
 	};
 
 	render() {
+		const { length: totalMovies } = this.state.movies;
+
+		if (!totalMovies) return <p>There are no movies in the database</p>;
 		return (
-			<table className="table">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Genre</th>
-						<th>Stock</th>
-						<th>Rate</th>
-						<th />
-					</tr>
-				</thead>
-				<tbody>
-					{this.state.movies.map(movie => (
-						<tr key={movie._id}>
-							<td>{movie.title}</td>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>{movie.dailyRentalRate}</td>
-							<td>
-								<button
-									onClick={() => this.handleDelete(movie._id)}
-									className="btn btn-danger btn-sm"
-								>
-									Delete
-								</button>
-							</td>
+			<React.Fragment>
+				<p>Showing {totalMovies} movies in the database</p>
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Genre</th>
+							<th>Stock</th>
+							<th>Rate</th>
+							<th />
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{this.state.movies.map(movie => (
+							<tr key={movie._id}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td>{movie.numberInStock}</td>
+								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<button
+										onClick={() => this.handleDelete(movie._id)}
+										className="btn btn-danger btn-sm"
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
 		);
 	}
 }
