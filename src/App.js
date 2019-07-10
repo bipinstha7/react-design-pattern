d29307c5c68f005b538f037be5bc00144eaf2851
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-10 22:51:52 +0545

    Counter app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<h1>App</h1>
-		</div>
-	);
+class App extends Component {
+	state = {
+		count: 0,
+	};
+
+	formatCount() {
+		const { count } = this.state;
+
+		return count === 0 ? 'Zero' : count;
+	}
+
+	getBadgeClasses() {
+		let classes = 'badge m-2 badge-';
+		classes += this.state.count === 0 ? 'warning' : 'primary';
+		return classes;
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
+			</div>
+		);
+	}
 }
 
 export default App;
