75fef22cfd234868b419711fa59c7f7eba74c47c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-11 22:39:07 +0545

    Change class based like component to stateless functional component as it doesn't has the states

--- src/components/common/like.js
+++ src/components/common/like.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Like extends Component {
-	render() {
-		const { liked, onClick } = this.props;
-		let classes = 'fa fa-heart';
-		if (!liked) classes += '-o';
-		return (
-			<i style={{ cursor: 'pointer' }} className={classes} onClick={onClick} />
-		);
-	}
-}
+const Like = ({ liked, onClick }) => {
+	let classes = 'fa fa-heart';
+	if (!liked) classes += '-o';
+	return (
+		<i style={{ cursor: 'pointer' }} className={classes} onClick={onClick} />
+	);
+};
 
 export default Like;
