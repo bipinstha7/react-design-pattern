1d6975e90fb541109a748e7eab7826b0bfe976cf
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-22 21:51:35 +0545

    Show active status on active paginate

--- src/components/common/pagination.js
+++ src/components/common/pagination.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import range from 'lodash/range';
 
-const Pagination = ({ totalMovies, pageSize }) => {
+const Pagination = props => {
+	const { totalMovies, pageSize, currentPage, onPageChange } = props;
 	const pagesCount = Math.ceil(totalMovies / pageSize);
 
 	// Don't show pagination for just 1 page
@@ -12,8 +13,13 @@ const Pagination = ({ totalMovies, pageSize }) => {
 		<nav>
 			<ul className="pagination">
 				{pages.map(page => (
-					<li className="page-item" key={page}>
-						<a className="page-link">{page}</a>
+					<li
+						className={page === currentPage ? 'page-item active' : 'page-item'}
+						key={page}
+					>
+						<a className="page-link" onClick={() => onPageChange(page)}>
+							{page}
+						</a>
 					</li>
 				))}
 			</ul>
