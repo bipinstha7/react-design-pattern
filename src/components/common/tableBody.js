f47be5cac03692cc193e087c48a834202c0c7793
bipinstha7 <bipin.shrestha901@gmail.com>
2019-07-22 23:47:03 +0545

    Sepaarate component for table body

---
+++ src/components/common/tableBody.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Like from './like';
+import get from 'lodash/get';
+
+class TableBody extends Component {
+	renderCell = (item, column) => {
+		if (column.content) {
+			return column.content(item);
+		}
+
+		return get(item, column.path);
+	};
+
+	render() {
+		const { data, columns } = this.props;
+
+		return (
+			<tbody>
+				{data.map(item => (
+					<tr key={item._id}>
+						{columns.map(column => (
+							<td key={column.label || column.key}>
+								{this.renderCell(item, column)}
+							</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		);
+	}
+}
+
+export default TableBody;
