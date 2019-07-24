8dee2cdae9e088308745d84dcb2deb036de02220
Bipin Shrestha <bipin.shrestha901@gmail.com>
2019-07-24 17:39:48 +0545

    Move reusable Input code to form component

--- src/components/common/form.js
+++ src/components/common/form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
+import Input from './input';
 
 class Form extends Component {
 	state = {
@@ -55,6 +56,29 @@ class Form extends Component {
 		data[target.name] = target.value;
 		this.setState({ data, errors });
 	};
+
+	renderButton(label) {
+		return (
+			<button disabled={this.validate()} className="btn btn-primary">
+				{label}
+			</button>
+		);
+	}
+
+	renderInput(name, label, type = 'text') {
+		const { data, errors } = this.state;
+
+		return (
+			<Input
+				type={type}
+				name={name}
+				label={label}
+				value={data[name]}
+				onChange={this.handleChange}
+				error={errors[name]}
+			/>
+		);
+	}
 }
 
 export default Form;
