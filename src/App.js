910e57681d64a00146d7bffafc1ab2baa8b811ff
Bipin Shrestha <bipin.shrestha901@gmail.com>
2019-07-26 14:59:59 +0545

    App.js   - Protect routes   - Show user's name in navbar

--- src/App.js
+++ src/App.js
@@ -12,19 +12,33 @@ import NavBar from './components/navBar';
 import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
+import Logout from './components/logout';
+import auth from './services/authService';
+import ProtectedRoute from './components/common/protectedRoute';
 
 class App extends Component {
+	state = {};
+
+	componentDidMount() {
+		const user = auth.getCurrentUser();
+		this.setState({ user });
+	}
+
 	render() {
 		return (
 			<Fragment>
 				<ToastContainer />
-				<NavBar />
+				<NavBar user={this.state.user} />
 				<main className="container">
 					<Switch>
 						<Route path="/login" component={LoginForm} />
+						<Route path="/logout" component={Logout} />
 						<Route path="/register" component={RegisterForm} />
-						<Route path="/movies/:id" component={MovieForm} />
-						<Route path="/movies" component={Movies} />
+						<ProtectedRoute path="/movies/:id" component={MovieForm} />
+						<Route
+							path="/movies"
+							render={props => <Movies {...props} user={this.state.user} />}
+						/>
 						<Route path="/customers" component={Customers} />
 						<Route path="/rentals" component={Rentals} />
 						<Route path="/not-found" component={NotFound} />
