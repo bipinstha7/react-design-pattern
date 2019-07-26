935bfc742b860597256130652dc5d1c4d2fe73b8
Bipin Shrestha <bipin.shrestha901@gmail.com>
2019-07-26 14:55:56 +0545

    Register new user

--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import Joi from 'joi-browser';
 
 import Form from './common/form';
+import { register } from '../services/userService';
+import auth from '../services/authService';
 
 class RegisterForm extends Form {
 	state = {
@@ -25,8 +27,21 @@ class RegisterForm extends Form {
 			.label('Name'),
 	};
 
-	doSubmit = () => {
-		console.log('submitted');
+	doSubmit = async () => {
+		try {
+			const response = await register(this.state.data);
+			auth.loginWithJwt(response.headers['x-auth-token']);
+
+			// this.props.history.push('/');
+			window.location = '/';
+		} catch (error) {
+			if (error.response && error.response.status === 400) {
+				const errors = { ...this.state.errors };
+				errors.username = error.response.data;
+
+				this.setState({ errors });
+			}
+		}
 	};
 
 	render() {
