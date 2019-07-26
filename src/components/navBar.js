33f0c8c2ad3ed48fc9901a2591fbe223240c8099
Bipin Shrestha <bipin.shrestha901@gmail.com>
2019-07-26 14:56:12 +0545

    On Navbar:   - Show login and regiter button if user is not logged in   - Show user name and logout button otherwise

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<Link className="navbar-brand" to="/">
@@ -29,9 +29,26 @@ const NavBar = () => {
 					<NavLink className="nav-item nav-link" to="/rentals">
 						Rentals
 					</NavLink>
-					<NavLink className="nav-item nav-link" to="/login">
-						Login
-					</NavLink>
+					{!user && (
+						<Fragment>
+							<NavLink className="nav-item nav-link" to="/login">
+								Login
+							</NavLink>
+							<NavLink className="nav-item nav-link" to="/register">
+								Register
+							</NavLink>
+						</Fragment>
+					)}
+					{user && (
+						<Fragment>
+							<NavLink className="nav-item nav-link" to="/profile">
+								{user.name}
+							</NavLink>
+							<NavLink className="nav-item nav-link" to="/logout">
+								Logout
+							</NavLink>
+						</Fragment>
+					)}
 				</div>
 			</div>
 		</nav>
